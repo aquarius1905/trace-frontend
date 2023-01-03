86be66e9c5cdb99007f8b82fa582218bf41754fc
reiko_iguchi <reiko6122@gmail.com>
2023-01-03 22:14:08 +0900

    2023/01/03

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 import LoginAndRegisterView from '@/views/LoginAndRegisterView.vue'
+import EmailVerifiedView from '@/views/Email/VerifiedView.vue'
 
 const routes = [
   {
@@ -12,7 +13,12 @@ const routes = [
     path: '/login-and-register',
     name: 'LoginAndRegisterView',
     component: LoginAndRegisterView
-  }
+  },
+  {
+    path: '/email/verified',
+    name: 'EmailVerifiedView',
+    component: EmailVerifiedView
+  },
 ]
 
 const router = createRouter({
---
+++ src/views/Email/VerifiedView.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <TextMessage :msg="メール認証が完了しました"></TextMessage>
+  </div>
+</template>
+
+<script>
+import TextMessage from "@/components/TextMessage";
+export default {
+  components: { TextMessage }
+};
+</script>
