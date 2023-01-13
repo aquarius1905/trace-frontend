f229aec7c273ffcdf6063ed9584f9dabf0b30e05
reiko_iguchi <reiko6122@gmail.com>
2023-01-13 22:05:25 +0900

    2023/01/13

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -17,16 +17,49 @@
         </div>
       </div>
     </div>
+    <div class="paginate__wrap">
+      <Paginate 
+      :v-model="current_page" 
+      :page-count="pageCount" 
+      :prev-text="'前へ'" :next-text="'次へ'"
+      :click-handler="paginateClickCallback" 
+      :container-class="'pagination'"
+      :page-class="'page-item'" 
+      :page-link-class="'page-link'"
+      :prev-class="'page-item'"
+      :prev-link-class="'page-link'"
+      :next-class="'page-item'"
+      :next-link-class="'page-link'"
+      :first-last-button="true" 
+      :first-button-text="'最初'"
+      :last-button-text="'最後'" />
+    </div>
   </div>
 </template>
 
 <script>
 import { api } from '@/plugins/axios'
+import VuejsPaginateNext from "vuejs-paginate-next"
 
 export default {
+  components: {
+    Paginate: VuejsPaginateNext
+  },
   data() {
     return {
-      products: []
+      products: [],
+      current_page: 1,
+      per_page: 8
+    }
+  },
+  computed: {
+    pageCount() {
+      return Math.ceil(this.products.length / this.per_page);
+    }
+  },
+  methods: {
+    paginateClickCallback(pageNum) {
+      this.current_page = Number(pageNum);
     }
   },
   async created() {
@@ -92,5 +125,47 @@ export default {
 
 .product__name{
   font-weight: normal;
+}
+</style>
+
+<style lang="css">
+.pagination {
+  display: flex;
+  justify-content: center;
+  color: #2D383C;
+}
+
+.page-item {
+  box-sizing: border-box;
+}
+
+.page-link {
+  cursor: pointer;
+  position: relative;
+  display: block;
+  padding: 0.5rem 0.75rem;
+  margin-left: -1px;
+  line-height: 1.25;
+  background-color: #FFF;
+  border: 1px solid #80a31d;
+}
+
+.page-item.disabled .page-link {
+  color: #BDBDBD;
+  pointer-events: none;
+  cursor: auto;
+  background-color: #FFF;
+  border: 1px solid #80a31d;
+}
+
+.page-item.active .page-link {
+  z-index: 3;
+  color: #FFF;
+  background-color: #80a31d;
+  border-color: #80a31d;
+}
+
+ul {
+  list-style: none;
 }
 </style>
